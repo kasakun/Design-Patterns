49b3fa90804970ccbcd019e3096f97ad9b8ba073
kasakun <chenzy@gatech.edu>
2020-03-30 20:15:30 -0700

    change to composite commands.

--- Command/BankAccount/src/main.cc
+++ Command/BankAccount/src/main.cc
@@ -1,37 +1,40 @@
 #include <iostream>
 #include <string>
+#include <memory>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 struct BankAccount
 {
     int balance = 0;
-    int limit = -500;
+    int overdraft_limit = -500;
 
     void deposit(int amount)
     {
         balance += amount;
-        cout << "deposited " << amount << ", balance now "
-            << balance << "\n";
+        cout << "deposited " << amount << ", balance now " <<
+            balance << "\n";
     }
 
-    void withdraw(int amount)
+    bool withdraw(int amount)
     {
-        if (balance - amount >= limit)
+        if (balance - amount >= overdraft_limit)
         {
             balance -= amount;
-            cout << "withdrew " << amount << ", balance now "
-                << balance << "\n";
+            cout << "withdrew " << amount << ", balance now " <<
+                balance << "\n";
+            return true;
         }
+        return false;
     }
 };
 
 struct Command
 {
-    virtual ~Command() = default;
-    virtual void call() const = 0;
-    virtual void undo() const = 0;
+    bool succeeded;
+    virtual void call() = 0;
+    virtual void undo() = 0;
 };
 
 // should really be BankAccountCommand
@@ -41,82 +44,124 @@ struct BankAccountCommand : Command
     enum Action { deposit, withdraw } action;
     int amount;
 
-    BankAccountCommand(BankAccount& account, const Action action, const int amount)
-        : account(account), action(action), amount(amount) {}
+    BankAccountCommand(BankAccount& account, const Action action,
+            const int amount)
+        : account(account),
+        action(action), amount(amount)
+    {
+        succeeded = false;
+    }
 
-    void call() const override
+    void call() override
     {
         switch (action)
         {
             case deposit:
                 account.deposit(amount);
+                succeeded = true;
                 break;
             case withdraw:
-                account.withdraw(amount);
+                succeeded = account.withdraw(amount);
                 break;
-            default: break;
         }
     }
 
-    void undo() const override
+    void undo() override
     {
+        if (!succeeded) return;
+
         switch (action)
         {
             case withdraw:
-                account.deposit(amount);
+                if (succeeded)
+                    account.deposit(amount);
                 break;
             case deposit:
                 account.withdraw(amount);
-                break;
-            default:
                 break;
         }
     }
 };
 
 // vector doesn't have virtual dtor, but who cares?
-struct CompositeBankAccountCommand
-  : vector<BankAccountCommand>, Command
+struct CompositeBankAccountCommand : vector<BankAccountCommand>, Command
 {
-  CompositeBankAccountCommand(const initializer_list<value_type>& items)
-    : vector<BankAccountCommand>(items) {}
+    CompositeBankAccountCommand(const initializer_list<value_type>& _Ilist)
+        : vector<BankAccountCommand>(_Ilist)
+    {
+    }
 
-  void call() const override
-  {
-    for (auto& cmd : *this)
-      cmd.call();
-  }
+    void call() override
+    {
+        for (auto& cmd : *this)
+            cmd.call();
+    }
 
-  void undo() const override
-  {
-    for (auto& cmd : *this)
-      cmd.undo();
-  }
+    void undo() override
+    {
+        for (auto it = rbegin(); it != rend(); ++it)
+            it->undo();
+    }
 };
+
+struct DependentCompositeCommand : CompositeBankAccountCommand
+{
+    explicit DependentCompositeCommand(const initializer_list<value_type>& _Ilist)
+        : CompositeBankAccountCommand{ _Ilist } {}
+
+    void call() override
+    {
+        bool ok = true;
+        for (auto& cmd : *this)
+        {
+            if (ok)
+            {
+                cmd.call();
+                ok = cmd.succeeded;
+            }
+            else
+            {
+                cmd.succeeded = false;
+            }
+        }
+    }
+};
+
+struct MoneyTransferCommand : DependentCompositeCommand
+{
+    MoneyTransferCommand(BankAccount& from, BankAccount& to, int amount)
+        : DependentCompositeCommand
+    {
+        BankAccountCommand{from, BankAccountCommand::withdraw, amount},
+            BankAccountCommand{to, BankAccountCommand::deposit, amount}
+    }
+    {
+    }
+};
+
 
 int main()
 {
     BankAccount ba;
     /*vector<BankAccountCommand> commands{*/
-    CompositeBankAccountCommand commands
-    {
-        BankAccountCommand{ba, BankAccountCommand::deposit, 100},
-        BankAccountCommand{ba, BankAccountCommand::withdraw, 200}
+    CompositeBankAccountCommand commands{
+        BankAccountCommand{ ba, BankAccountCommand::deposit, 100 },
+            BankAccountCommand{ ba, BankAccountCommand::withdraw, 200 }
     };
 
     cout << ba.balance << endl;
 
     // apply all the commands
     /*for (auto& cmd : commands)
-    {
-    cmd.call();
-    }*/
+      {
+      cmd.call();
+      }*/
     commands.call();
 
     cout << ba.balance << endl;
 
     /*for_each(commands.rbegin(), commands.rend(),
-    [](const BankAccountCommand& cmd) { cmd.undo(); });*/
+      [](const BankAccountCommand& cmd) { cmd.undo(); });*/
     commands.undo();
 
     cout << ba.balance << endl;
